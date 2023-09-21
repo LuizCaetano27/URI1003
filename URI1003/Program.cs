8a033b3b9143442e75fff5c35f2956bf65e2763a
luiz.caetano <luiz.caetano@MMQ003477>
2023-09-21 16:33:48 -0300

    Adicionar arquivos de projeto.

---
+++ URI1003/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int A, B,soma;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            soma = A + B;
+
+            Console.WriteLine("SOMA = " + (soma));
+
+            Console.ReadLine();
+        }
+    }
+}
